f11d85f9c7f76e0e168baa7a007f784ff55bb605
yogeswar <yogi2198@gmail.com>
2019-06-19 08:09:40 +0530

    Using Sequelize ORM CRUD operation in cart and displayed them as orders.

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', 'Yogi2198', {
   dialect: 'mysql',
   host: 'localhost'
 });
