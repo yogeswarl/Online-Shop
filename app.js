40131ebb04898ce7adb0481545225b17f8c834b0
yogeswar <yogi2198@gmail.com>
2020-02-09 20:35:22 +0530

    wrote the Database in MongoDb. added the show and add functionality

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart')
-const CartItem  = require('./models/cart-items')
-const Order = require('./models/order')
-const OrderItem  = require('./models/order-items')
+const mongoConnect = require('./util/database').mongoConnect;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,45 +17,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next)=> {
-  User.findByPk(1).then(user => {
-    req.user =user;
-    next();
-  })
-  .catch(err => console.log(err));
+  // User.findByPk(1).then(user => {
+  //   req.user =user;
+  //   next();
+  // })
+  // .catch(err => console.log(err));
+  next()
 })
 
-app.use('/admin', adminRoutes);
+ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
-Product.belongsTo(User, { constraints: true, onDelete:'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through : CartItem});
-Product.belongsToMany(Cart, {through : CartItem});
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product,{through: OrderItem});
-sequelize
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-  })
-  .then(user => {
-    if(!user){
-      return User.create({name:'yogesh', email:'test@test.com'});
-    }
-    return user;
-  }).then(user => {
-    return user.createCart();
-  })
-  .then(cart => {
-    // console.log(result);
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+ mongoConnect(client =>{
+   console.log(client);
+   app.listen(3000);
+ });
--- util/database.js
+++ util/database.js
@@ -1,8 +1,26 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'Yogi2198', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    "mongodb+srv://yogeswar:Yogi2198@cluster-test-p9hqd.mongodb.net/shop?retryWrites=true&w=majority"
+    ).then(client =>{
+      console.log('connected!');
+      _db = client.db()
+      callback();
+    }).catch(error=>{
+      console.log(error);
+      throw error;
+    });
+  }
+
+  const getDb =() =>{
+    if(_db){
+      return _db;
+    }
+    throw 'No Database found';
+  }
+  exports.mongoConnect = mongoConnect;
+  exports.getDb = getDb;
