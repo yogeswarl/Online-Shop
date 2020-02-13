f72497481597c7d13c0bb4655ee8f8aa6e5f799e
yogeswar <yogi2198@gmail.com>
2020-02-13 08:03:42 +0530

    deleting a  product

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
 		this.price = price;
 		this.imageUrl = imageUrl;
 		this.description = description;
-		this._id =  new mongodb.ObjectID(id);
+		this._id = id ? new mongodb.ObjectID(id):null; //checks if id available else sets to null  
 	}
 
 	save() {
@@ -55,6 +55,18 @@ class Product {
 			.then((product) => {
 				console.log(product);
 				return product;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+	static deleteById(prodId) {
+		const db = getDb();
+		return db
+			.collection("products")
+			.deleteOne({ _id: new mongodb.ObjectId(prodId) })
+			.then((result) => {
+				console.log('Deleted');
 			})
 			.catch((err) => {
 				console.log(err);
