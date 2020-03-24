5f79e8e0babff84431e7f6c3030be76a7b54d4c8
yogeswar <yogi2198@gmail.com>
2020-03-24 08:55:11 +0530

    Simple addition of cookies for authentication

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+exports.getLogin = (req, res, next) => {
+	const isLoggedIn = req.get('Cookie').trim().split('=')[1];
+	res.render("auth/login", {
+		pageTitle: "Login",
+		path: "/login",
+		isAuthenticated: isLoggedIn
+	});
+};
+exports.postLogin = (req, res, next) => {
+	res.setHeader('Set-Cookie','loggedIn=true');
+	res.redirect('/')
+};
