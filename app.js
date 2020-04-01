3d9036c9a294822cff203ce906c3791f8e9b9cee
yogeswar <yogi2198@gmail.com>
2020-04-01 21:57:06 +0530

    csrf token added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const mongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require('csurf');
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -12,10 +13,12 @@ const User = require("./models/user");
 const MONGODB_URI =
 	"mongodb+srv://yogeswar:Yogi2198@cluster-test-p9hqd.mongodb.net/shop";
 const app = express();
+
 const store = new mongoDBStore({
 	uri: MONGODB_URI,
 	collection: "sessions"
 });
+const csrfProtect = csrf();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -33,6 +36,7 @@ app.use(
 		store: store
 	})
 );
+app.use(csrfProtect)
 
 app.use((req, res, next) => {
 	if (!req.session.user) {
@@ -45,6 +49,11 @@ app.use((req, res, next) => {
 		})
 		.catch((err) => console.log(err));
 });
+app.use((req,res,next) => {
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken= req.csrfToken();
+	next();
+})
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
