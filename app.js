994a6624e77af031fac8c61629d8a56c4291fe0f
yogeswar <yogi2198@gmail.com>
2020-04-09 07:39:30 +0530

    handling errors in my project

--- app.js
+++ app.js
@@ -37,28 +37,47 @@ app.use(
 app.use(csrfProtect)
 app.use(flash());
 
-app.use((req, res, next) => {
-	if (!req.session.user) {
-		return next();
-	}
-	User.findById(req.session.user._id)
-		.then((user) => {
-			req.user = user;
-			next();
-		})
-		.catch((err) => console.log(err));
-});
 app.use((req,res,next) => {
 	res.locals.isAuthenticated = req.session.isLoggedIn;
 	res.locals.csrfToken= req.csrfToken();
 	next();
 })
 
+app.use((req, res, next) => {
+	// throw new Error('dummy') synchronous errors can be thrown this way
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
+		.then((user) => {
+			if(!user){
+				return next();
+			}
+			req.user = user;
+			next();
+		})
+		.catch((err) => {
+			next(new Error(err)); //Inside async code you need to wrap it inside next()
+		});
+});
+
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500',errorController.get500);
 app.use(errorController.get404);
+
+app.use((error,req,res,next)=>{
+	res
+		.status(500)
+		.render("500", {
+			pageTitle: "Error!",
+			path: "/500",
+			isAuthenticated: req.session.isLoggedIn,
+		});
+})
 
 mongoose
 	.connect(process.env.DB_SERVER, { useNewUrlParser: true, useUnifiedTopology: true })
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const { validationResult } = require('express-validator');
 const Product = require("../models/product");
 
@@ -53,7 +55,9 @@ exports.postAddProduct = (req, res, next) => {
 			res.redirect("/admin/products");
 		})
 		.catch((err) => {
-			console.log(err);
+			const error = new Error(err)
+			error.httpStatusCode = 500;
+			return next(error);
 		});
 };
 
@@ -78,7 +82,11 @@ exports.getEditProduct = (req, res, next) => {
 				validationErrors: []
 			});
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			const error = new Error(err)
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -120,7 +128,12 @@ exports.postEditProduct = (req, res, next) => {
 				console.log("UPDATED PRODUCT!");
 				res.redirect("/admin/products");
 			})
-		}).catch((err) => console.log(err));
+		})
+		.catch((err) => {
+			const error = new Error(err)
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
 
 exports.getProducts = (req, res, next) => {
@@ -132,7 +145,11 @@ exports.getProducts = (req, res, next) => {
 				path: "/admin/products",
 			});
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			const error = new Error(err)
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -142,5 +159,9 @@ exports.postDeleteProduct = (req, res, next) => {
 			console.log("DESTROYED PRODUCT");
 			res.redirect("/admin/products");
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			const error = new Error(err)
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
