1279763f0b84f1d1868d3ab172ff33b1e05fa2d2
yogeswar <yogi2198@gmail.com>
2020-04-11 13:29:54 +0530

    file upload and download with authentication

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const path = require('path');
-
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
-require('dotenv').config();
 const flash = require('connect-flash');
 const multer = require('multer');
 
